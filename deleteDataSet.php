ec584a919a69e680a979435bbaed1c03df132e3a
Bruno Freiberger Garcia <freibergergarcia.com>
2019-10-09 16:50:56 +1100

    Cleaning the crap

--- deleteDataSet.php
+++ deleteDataSet.php
@@ -6,26 +6,15 @@ use Acme\DeleteOfflineConversion;
 use Acme\FacebookConnector;
 use Symfony\Component\Dotenv\Dotenv;
 
-
-$test = explode("-", "CKbrunogarcia@pvhba.com", 2);
-
-echo (count($test) > 1) ? $test[1] : $test[0];
-
-var_dump($test);
-
-exit;
-
-
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env', __DIR__ . '/.env.dev');
 
 $filePath = __DIR__ . '/assets/csv/DEFAULT-DEMO.csv';
-
 $file = new Csv($filePath);
 $facebook = new FacebookConnector();
 
 $offlineConversion = new DeleteOfflineConversion($facebook);
-$offlineConversion->setDataSet('531745504239124')
+$offlineConversion->setDataSet('{REPLACE WITH THE DATASET ID}')
     ->sendRequest();
 
 $results = $offlineConversion->getResponse();
